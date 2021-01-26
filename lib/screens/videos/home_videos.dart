e8d43d957afdc7f01903a8793da01b0c2a5125f1
godfatherdemon <momochan_suki@yahoo.com>
2021-01-26 16:05:14 +0700

    init

--- lib/screens/videos/home_videos.dart
+++ lib/screens/videos/home_videos.dart
@@ -56,10 +56,10 @@ class HomeVideosState extends State<HomeVideos> {
     //     'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1226-large.mp4');
     // await _videoPlayerController2.initialize();
     _videoPlayerController1 = VideoPlayerController.network(
-        'https://produk.lintasjabarindo.com/wp-content/uploads/2021/01/Coming-Soon-LSJ-TV.mp4');
+        'https://produk.lintasjabarindo.com/wp-content/uploads/2021/01/');
     await _videoPlayerController1.initialize();
     _videoPlayerController2 = VideoPlayerController.network(
-        'https://produk.lintasjabarindo.com/wp-content/uploads/2021/01/Coming-Soon-LSJ-TV.mp4');
+        'https://produk.lintasjabarindo.com/wp-content/uploads/2021/01/');
     await _videoPlayerController2.initialize();
     _chewieController = ChewieController(
       videoPlayerController: _videoPlayerController1,
